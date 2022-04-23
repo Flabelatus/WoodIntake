ddb495e4aeef25c2f977e56dbd24dc855600f66b
Flabelatus <98832679+Flabelatus@users.noreply.github.com>
2022-04-24 01:02:54 +0200

    updates and datawood csv file

--- digitalintake.py
+++ digitalintake.py
@@ -2,6 +2,7 @@
 import pandas as pd
 # import numpy as np
 import streamlit as st
+from os import getcwd
 
 
 class DigitalIntake:
@@ -9,7 +10,7 @@ class DigitalIntake:
 
     def __init__(self, name):
         self.name = name
-        self.data = pd.read_csv(self.name, usecols=[
+        self.fields = [          
             "Index",
             "Width",
             "Length",
@@ -19,8 +20,9 @@ class DigitalIntake:
             "Indexed",
             "Density",
             "Weight"
-        ], delimiter=',')
-        self.fields = [item for item in self.data]
+        ]
+        self.data = pd.read_csv(self.name, usecols=self.fields, delimiter=',')
+
 
     def __str__(self):
         return f"Data base {self.name}, containing the data in CSV format."
@@ -37,17 +39,7 @@ class DigitalIntake:
 
     @st.cache
     def fetch(self, row=None):
-        fetched_data = pd.read_csv(self.name, nrows=row, usecols=[
-            "Index",
-            "Width",
-            "Length",
-            "Height",
-            "Type",
-            "Color",
-            "Indexed",
-            "Density",
-            "Weight"
-        ])
+        fetched_data = pd.read_csv(self.name, nrows=row, usecols=self.fields)
         return fetched_data
 
     @st.cache
@@ -68,12 +60,11 @@ class DigitalIntake:
 
 
 if __name__ == "__main__":
-    directory = 'C:\\Users\\scanlab\\Documents\\' \
-                'HvA\\DATA-WOOD\\Test Folder CSV\\'
-    csv_file = 'Datawood.csv'
+    directory = getcwd()
+    csv_file = '/Datawood.csv'
     filename = directory + csv_file
     wood_intake = DigitalIntake(filename)
-    wood_intake.display_column(head=10)
+    wood_intake.display_column(head=3)
     info = wood_intake.convert()
 
     # Streamlit on Local URL: http://localhost:8501
@@ -81,25 +72,31 @@ if __name__ == "__main__":
     st.text("The following table demonstrates real-time data captured by\n"
             "the digital intake process in the Robot Lab as means of building\n"
             "a data base of residual wood.")
+
     st.subheader("Digital Intake Results (Waste Wood from CW4N)")
     st.write(info)
     st.subheader(f'TOTAL Number of wood scanned: {len(info["Index"])}\n\n')
-    st.download_button('Download Table', str(info))
+    st.download_button('Download Table', str(info), mime='text/csv')
 
     length_values = info['Length']
     st.subheader('Length Distribution in mm\n')
     st.bar_chart(length_values)
 
-    st.subheader("Filter Desired Pieces Based on Length")
-    selected_size = st.slider('Length in mm', min(info['Length']), max(info['Length'] + 1))
+    criteria = 'Length'
+    st.subheader(f"Filter Desired Pieces Based on {criteria}")
+    slider_min_val = min(sorted(info[criteria]))
+    slider_max_val = max(sorted(info[criteria]))
+    selected_size = st.slider('Length in mm', value=sorted())
     st.write("The Items in the table are the ID values"
              " of the pieces under the selected length")
+    st.write(selected_size)
 
     data = None
-    for index, sizes in enumerate(sorted(info['Length'])):
+    for index, sizes in enumerate(sorted(info[criteria])):
         if sizes < selected_size:
             data = wood_intake.fetch(index)
 
     selected_df = pd.DataFrame(data)
+    print(selected_df)
     st.write(selected_df)
-    st.download_button('Download Selection', str(data))
+    st.download_button('Download Selection', str(data), mime='text/csv')
